e8e12a20670ecf5509e519c6337845639266f6cd
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-05-08 09:29:19 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@
  * const.letについて
  */
 
-var val1 = "var変数";
+// var val1 = "var変数";
 
-console.log(val1);
+// console.log(val1);
 
-val1 = "上書き";
-console.log(val1);
+// val1 = "上書き";
+// console.log(val1);
 
-var val1 = "kakikukeko";
-console.log(val1);
+// var val1 = "kakikukeko";
+// console.log(val1);
 
-let val2 = "rabbit";
-console.log(val2);
+// let val2 = "rabbit";
+// console.log(val2);
 
-val2 = "panda";
-console.log(val2);
+// val2 = "panda";
+// console.log(val2);
 
-const var3 = "banana";
-console.log(var3);
+// const var3 = "banana";
+// console.log(var3);
 
-const var4 = {
-  name: "anna",
-  age: 28
-};
-var4.name = "yuna";
-var4.hobby = "sing";
-console.log(var4);
+// const var4 = {
+//   name: "anna",
+//   age: 28
+// };
+// var4.name = "yuna";
+// var4.hobby = "sing";
+// console.log(var4);
 
-const var5 = ["dog", "bird"];
-var5[0] = "elephant";
-var5.push("cat");
-console.log(var5);
+// const var5 = ["dog", "bird"];
+// var5[0] = "elephant";
+// var5.push("cat");
+// console.log(var5);
 
 /**
  * varは一昔前の変数定義方法。上書き、再定義可。
@@ -52,3 +52,15 @@ console.log(var5);
  *
  * 自薦の場ではほぼconstを使う。
  */
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "anna";
+const age = 28;
+
+console.log("私の名前は" + name + "です。年齢は" + age + "です。");
+console.log(`私の名前は${name}です。年齢は${age}です。`);
+
+// バッククォートを使うと文字列に変数を入れ込むような者が見やすくなる。
