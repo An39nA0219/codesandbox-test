11b2e9c73cea4370144b6ab6efe6f9bb0b32f721
Ankichi <59509443+An39nA0219@users.noreply.github.com>
2021-05-09 14:08:15 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -69,25 +69,63 @@
  * アロー関数
  */
 // 従来の書き方
-function func1(str) {
-  return str;
-}
-console.log(func1("hello"));
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("hello"));
 
-const func2 = function (str) {
-  return str;
+// const func2 = function (str) {
+//   return str;
+// };
+
+// console.log(func2("ciao"));
+
+// //  アロー関数
+// const func3 = (str) => {
+//   return str;
+// };
+
+// console.log(func3("こんにちは"));
+
+// // アロー関数は、処理が1行の場合は、return不要、また{}も不要
+
+// const func4 = (num1, num2) => num1 + num2;
+// console.log(func4(10, 20));
+
+/**
+ * 分割代入
+ */
+
+/**
+ * 多分、分割代入って、基本的には配列とか、ハッシュとか、同じまとまりになっているものを、
+ * 一括で分けてそれぞれを変数に入れるということっぽい！
+ */
+
+// 従来
+const myprofile = {
+  name: "uchida",
+  age: 29
 };
 
-console.log(func2("ciao"));
+const message = `${myprofile.name}は${myprofile.age}歳です`;
+console.log(message);
 
-//  アロー関数
-const func3 = (str) => {
-  return str;
-};
+//  分割代入
+// ↓は、myprofileのハッシュを順番に、name,ageという変数にそれぞれ入れてあげるということ
+//  myprofileの中にはnameオブジェクトとageオブジェクトがある。
+// nameはnameという変数に代入、ageはageという変数に台有することができるが、
+// 例えば、nameをname1とかオブジェクト名と違う変数に入れようとするとエラーになる。
+const { name, age } = myprofile;
+const message1 = `${name}は${age}歳です`;
+console.log(message1);
 
-console.log(func3("こんにちは"));
+//  配列の分割代入
+// 従来
+const array = ["anna", "yuna", "reina"];
+const message2 = `${array[0]}と${array[1]}と${array[2]}は兄弟です`;
+console.log(message2);
 
-// アロー関数は、処理が1行の場合は、return不要、また{}も不要
-
-const func4 = (num1, num2) => num1 + num2;
-console.log(func4(10, 20));
+//  分割代入。配列の場合は、前から順番にこちらで指定した変数に入っていく
+const [name1, name2, name3] = array;
+const message3 = `${name1}と${name2}と${name3}は兄弟です`;
+console.log(message3);
